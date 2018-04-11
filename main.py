5933c8c97f2a1eb84490626dd1b74ad755fec03f
Dmitriy Stepanov <stdian@mail.ru>
2018-04-11 21:26:07 +0800

    Added choosing city and getting real data

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
 
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 from ezprint import p
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import datetime
+import requests
 import random
-
-date = []
-tem = np.random.randint(60, size=(7)) - 30
-for i in range(1, 8):
-	date.append('Feb-' + str(i))
-
-mas = np.arange((5)+2)
-df = pd.DataFrame({
-	'Temperature' : tem
-	}, index = date)
+import json
+import time
 
 
-if __name__ == '__main__':
-	p(df)
-	line_down, = plt.plot(df, label='Temperature change', c='green', lw=3.5, marker='h', mec='red')
-	plt.ylim(-30, 30)
-	plt.legend(handles=[line_down])
-	plt.title('London')
-	plt.xlabel('Date')
+latlon_url = 'https://api.opencagedata.com/geocode/v1/json?q='
+latlon_api_key = '&key=784aa1346e1d46c9ad32544372f7316e'
+weather_url = 'https://api.darksky.net/forecast/'
+weather_api_key = '08d603a88b0d98d0c6b24dfb41f716bc'
+
+
+def get_weather(latitude, longitude, city):
+	r = requests.get(weather_url + weather_api_key + '/' + str(latitude) + ',' + str(longitude)).text
+	jsn = json.loads(r)
+	temps = []
+	times = []
+	jsn = jsn['hourly']['data']
+	for i in jsn:
+		temps.append(fahrenheit_to_celsius(i['temperature']))
+		times.append(datetime.datetime.fromtimestamp(int(i['time'])))
+
+	pl = plt.plot(times, temps, label = 'Temperature change', c = 'green', lw = 3.5, marker = 'o')
+	plt.gcf().autofmt_xdate()
+	myFmt = mdates.DateFormatter('%H:%M')
+	plt.gca().xaxis.set_major_formatter(myFmt)
+	plt.legend(handles=[pl[0]])
+	plt.xlabel('Hours')
 	plt.ylabel('Temperature')
+	plt.title(city.capitalize())
 	plt.show()
-	plt.savefig('1.png')
-	p('Save graph!')
+
+
+def get_latlon(city):
+	r = requests.get(latlon_url + city + latlon_api_key).text
+	lljsn = json.loads(r)
+	longitude = lljsn['results'][0]['geometry']['lng']
+	latitude  = lljsn['results'][0]['geometry']['lat']
+	return [latitude, longitude]
+
+
+def fahrenheit_to_celsius(temp):
+	t = (temp - 32) / 1.8
+	return round(t, 2)
+
+
+def kelvin_to_celsius(temp):
+	t = temp - 273.15
+	return t
+
+
+city = 'irkutsk'
+latlon = get_latlon(city)
+
+get_weather(latlon[0], latlon[1], city)
